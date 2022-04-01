1527261a67da8f6fa21cdbae7d6f0c68e1d3cdfc
nanog0d <ani.nanwal92@gmail.com>
2022-04-01 20:28:25 +0530

    Added exception handling in controller

--- src/main/java/com/nano/todoapp/controller/RootController.java
+++ src/main/java/com/nano/todoapp/controller/RootController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nano.todoapp.pojo.CustomException;
 import com.nano.todoapp.pojo.Todo;
 import com.nano.todoapp.pojo.TxnStatus;
 import com.nano.todoapp.service.TodoService;
@@ -28,17 +29,17 @@ public class RootController {
 	}
 	
 	@GetMapping("/todo/{id}")
-	public Todo getTodo(@PathVariable int id) {
+	public Todo getTodo(@PathVariable int id) throws CustomException {
 		return todoService.getTodo(id);
 	}
 	
 	@PostMapping("/todo")
-	public TxnStatus addTodo(@RequestBody Todo todo) {
+	public TxnStatus addTodo(@RequestBody Todo todo) throws CustomException {
 		return todoService.addTodo(todo);
 	}
 	
 	@DeleteMapping("/todo/{id}")
-	public TxnStatus deleteTodo(@PathVariable int id) {
+	public TxnStatus deleteTodo(@PathVariable int id) throws CustomException {
 		return todoService.deleteTodo(id);
 	}
 }
